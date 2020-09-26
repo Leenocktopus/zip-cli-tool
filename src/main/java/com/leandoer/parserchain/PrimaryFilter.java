4db6869041fb5454cbfeea37b4be5b5a61faa481
Leandoer <mileenocktopus@gmail.com>
2020-09-26 20:54:16 +0300

    slightly modified filters. added proper instruction for -help command. improved ZipService

--- src/main/java/com/leandoer/parserchain/PrimaryFilter.java
+++ src/main/java/com/leandoer/parserchain/PrimaryFilter.java
@@ -16,18 +16,23 @@ public class PrimaryFilter extends CLIFilter {
 	protected void doFilterSpecific(Queue<String> args, CLIContext cliContext) {
 		String argument = args.peek();
 		if (argument == null || argument.equals("-help")) {
-			logger.info("-info");
-			logger.info("-version");
-			logger.info("-verbose");
-			logger.info("-a");
-			logger.info("-n");
-			logger.info("-u");
-			logger.info("-d");
-			logger.info("zip [-anud] [ZIP_NAME] [FILE1, FILE2, ..]");
+			logger.info(String.format("%6s zip [options] \n \t\t(to execute one of commands)", "Usage:"));
+			logger.info(String.format("%6s zip [options] [zip archive] [filename1, filename2, ...]\n \t\t(to execute archiver commands)", "or"));
+			logger.info("\nwhere commands include:\n");
+			logger.info(String.format("\t %s \t print help message to the output stream", "-info "));
+			logger.info(String.format("\t %s \t print product version and developer to the output and exit", "-version"));
+			logger.info("\nwhere options include:\n");
+			logger.info(String.format("\t %s \t enable verbose output", "-verbose"));
+			logger.info(String.format("\t %s \t enable debug output\n", "-debug"));
+			logger.info(String.format("\t %s \t create archive with the name provided as [zip-archive]\n\t\t and contents provided as [filename1, filename2, ...]\n", "-a"));
+			logger.info(String.format("\t %s \t unpack archive with the name provided as [zip-archive]\n\t\t and destination provided as [filename]. Using this option\n\t\t without destination will unpack files to the current directory.\n\t\t Multiple destinations produce exception.\n", "-n"));
+			logger.info(String.format("\t %s \t update archive with the name provided as [zip-archive]\n\t\t and contents provided as [filename1, filename2, ...]\n" ,"-u"));
+			logger.info(String.format("\t %s \t remove files provided as [filename1, filename2, ...]\n\t\t from archive provided [zip-archive]. Each filename \n\t\t should be an absolute path in the archive.\n","-d"));
 			this.next = null;
 		} else if (argument.equals("-version")) {
-			logger.info("Zip CLI Archiver - Version 0.0.1");
-			logger.info("Developed by Alexey Raichev in 2020");
+			logger.info("\tZIP CLI Archiver - Version 0.0.3");
+			logger.info("\tDeveloped by Alexey Raichev in 2020");
+			logger.info("\tmileenocktopus@gmail.com");
 			args.poll();
 			this.next = null;
 		}
